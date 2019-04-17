90538db095fff739679cd466925782fef82390e1
Adm <adm@MacBook-Air-de-Adm.local>
2019-04-17 19:49:50 -0300

    template

--- src/app/utils/app-init.ts
+++ src/app/utils/app-init.ts
@@ -5,7 +5,7 @@ import { environment } from '../../environments/environment';
 
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
   let keycloakConfig: KeycloakConfig = {
-    url: 'http://54.202.0.205:8080/auth',
+    url: 'http://ec2-54-202-0-205.us-west-2.compute.amazonaws.com:8080/auth',
     realm: 'master',
     clientId: 'my-app',
     credentials: {secret : '510b5325-24e4-475c-924b-22d03f35bdc7'}
